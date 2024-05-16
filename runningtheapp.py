f91e29ada63fe6d02f4aa0ac1aac3f92b2402068
creyjing <creyji@gmail.com>
2024-05-16 14:08:26 +0800

    Update runningtheapp.py

--- runningtheapp.py
+++ runningtheapp.py
@@ -11,7 +11,12 @@ Created on Thu May 16 09:59:57 2024
 
 #path package to find the local virtual address where streamlit clones the github repo to
 
+import streamlit as st
+import altair as alt
 
+import sqlite3
+
+import sys
 
 
 ##from langchain.embeddings import OpenAIEmbeddings
